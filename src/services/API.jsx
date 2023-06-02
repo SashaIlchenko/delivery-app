444715f592ff693b5316d6710e174dd73ce53f4e
SashaIlchenko <sandra.ilchenko11@gmail.com>
2023-06-02 15:26:33 +0300

    edit reqests

--- src/services/API.jsx
+++ src/services/API.jsx
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-const BASE_URL = 'http://localhost:3001/api/';
+const BASE_URL = 'http://localhost:3001/api';
 
 export async function getFood() {
-    const response = await axios.get(`${BASE_URL}products/`);
+    const response = await axios.get(`${BASE_URL}/products/`);
     return response.data;
 }
 
 export async function addOrder(values) {
-    const response = await axios.post(`${BASE_URL}orders/`, { ...values });
+    const response = await axios.post(`${BASE_URL}/orders/`, { ...values });
 
     return response.data;
 }
